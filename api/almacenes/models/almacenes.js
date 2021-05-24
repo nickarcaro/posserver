39d766f1ae00461c7501fee26d99270d0efcd6ba
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2021-05-24 13:04:16 -0400

    add slugcreation to store

--- api/almacenes/models/almacenes.js
+++ api/almacenes/models/almacenes.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
+const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.nombre) {
+        data.slug = slugify(data.nombre, { lower: true });
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.nombre) {
+        data.slug = slugify(data.nombre, { lower: true });
+      }
+    },
+  },
+};
