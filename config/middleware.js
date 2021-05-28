1cc4729cde572cc4e1c39cb3547ea0d8340dd2c1
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2021-05-27 22:54:34 -0400

    fix

--- config/middleware.js
+++ config/middleware.js
@@ -3,8 +3,8 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      headers: "*",
-      origin: ["https://posclient-2bjm7.ondigitalocean.app"],
+      //headers: "*",
+      origin: ["*"],
     },
   },
 };
