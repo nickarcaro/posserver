565676569ea07deda1858eae97407e1ee2bce9e2
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2021-05-27 22:24:54 -0400

    cors

---
+++ config/middleware.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //...
+  settings: {
+    cors: {
+      enabled: true,
+      // headers: '*',
+      origin: ["https://posclient-2bjm7.ondigitalocean.app"],
+    },
+  },
+};
