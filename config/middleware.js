e5fedd346cebc4d1501313bbc0dacbddf63504b9
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2021-06-05 03:31:22 -0400

    add cors policy

--- config/middleware.js
+++ config/middleware.js
@@ -6,8 +6,23 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      origin: ["*"],
-      headers: "application/json",
+      origin: "*",
+      expose: [
+        "WWW-Authenticate",
+        "Server-Authorization",
+        "Access-Control-Expose-Headers",
+      ],
+      maxAge: 31536000,
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "X-Frame-Options",
+        "Origin",
+        "Access-Control-Allow-Headers",
+        "access-control-allow-origin",
+      ],
     },
   },
 };
